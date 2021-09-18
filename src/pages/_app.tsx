32741b870cd427766c2d4f1e09e298aca5e7e148
Artur Santiago <artursantiago2001@hotmail.com>
2021-09-18 19:02:32 -0300

    set up pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-523.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="Boilerplate to work with Typescript, React, NextJS and Styled Components"
